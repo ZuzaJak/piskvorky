b15fb155619b78f9db0150831ae729463ad8b17d
Zuzana Jakubčáková <zuzkajakubcak@gmail.com>
2020-10-18 13:27:07 +0200

    podmínka

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
 'use strict';
+const circle = document.querySelector('.board__field--circle');
+const cross = document.querySelector('.board__field--cross');
+const buttons = document.querySelector('button');
+const hraje = document.querySelector('.white');
+
+/* hraje.classList.toggle('board__field--cross'); */
+
+const play = () => {
+  if (hraje === circle) {
+    buttons.className = 'board__field--circle';
+  } else {
+    buttons.className = 'board__field--cross';
+  }
+};
+
+buttons.addEventListener('click', play);
