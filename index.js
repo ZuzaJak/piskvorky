06514bcc25779a04c4ef29105e6d10de33d408fd
Zuzana Jakubčáková <zuzkajakubcak@gmail.com>
2020-10-18 19:18:18 +0200

    problem s buttons, přidává se stále jen třída s kolečkem

--- index.js
+++ index.js
@@ -4,11 +4,20 @@ const cross = document.querySelector('.board__field--cross');
 const buttons = document.querySelectorAll('button');
 const hraje = document.querySelector('.white');
 
+const tah = () => {
+  hraje.classList.toggle('board__field--circle');
+  hraje.classList.toggle('board__field--cross');
+};
+
+for (let i = 0; i < buttons.length; i += 1) {
+  buttons[i].addEventListener('click', tah);
+}
+
 const play = (event) => {
-  if (hraje === circle) {
-    event.target.classList.add('board__field--circle');
-  } else {
+  if (hraje === cross) {
     event.target.classList.add('board__field--cross');
+  } else {
+    event.target.classList.add('board__field--circle');
   }
 };
 
