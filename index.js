939aa47aa83d6a0f66e3e4ae4c9b0656768bcffe
Zuzana Jakubčáková <zuzkajakubcak@gmail.com>
2020-10-18 21:15:00 +0200

    zkrácení

--- index.js
+++ index.js
@@ -4,15 +4,8 @@ const cross = document.querySelector('.board__field--cross');
 const buttons = document.querySelectorAll('button');
 const hraje = document.querySelector('.white');
 
-const tah = () => {
-  hraje.classList.toggle('board__field--cross');
-};
-
-for (let i = 0; i < buttons.length; i += 1) {
-  buttons[i].addEventListener('click', tah);
-}
-
 const play = (event) => {
+  hraje.classList.toggle('board__field--cross');
   if (hraje === cross) {
     event.target.classList.add('board__field--cross');
   } else {
